d986bdcaa5252b20ce0c75fd547b24f43db01878
Ajinkya <ajinkya.a.rajguru@gmail.com>
2015-01-15 21:47:29 +0530

    Initial Message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+app=angular.module('main',['ngRoute']);
 
+app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+	$routeProvider.when("/",{
+		templateUrl:'templates/default.html',
+		controller:"PageCtrl"
+	})
+	.when("/about",{
+		templateUrl:'templates/page.html',
+		controller:"PageCtrl"
+	})
+	.when("/resume",{
+		templateUrl:'templates/page.html',
+		controller:"PageCtrl"
+	}).otherwise({ redirectTo: '/' });
+
+	$locationProvider.html5Mode(true);
+}]);
+
+
+app.controller('PageCtrl', ['$scope', function ($scope) {
+	console.log("Send my regards");
+}]);
